dc094a3a963f2357bd94c8908b81cabe46590513
Patrick Linnane <patrick@linnane.io>
2024-03-29 11:10:41 -0700

    ryujinx 1.1.1248 (new cask)
    Signed-off-by: Patrick Linnane <patrick@linnane.io>

---
+++ Casks/ryujinx.rb
@@ -0,0 +1,21 @@
+cask "ryujinx" do
+  version "1.1.1248"
+  sha256 "6941563e29814aa6e31a199da9ea628544270b40df98018349f0b6279960c4e8"
+
+  url "https://github.com/Ryujinx/release-channel-master/releases/download/#{version}/ryujinx-#{version}-macos_universal.app.tar.gz",
+      verified: "github.com/Ryujinx/release-channel-master/"
+  name "Ryujinx"
+  desc "Nintendo Switch emulator"
+  homepage "https://ryujinx.org/"
+
+  depends_on macos: ">= :monterey"
+
+  app "Ryujinx.app"
+
+  zap trash: [
+    "~/Library/Application Support/Ryujinx",
+    "~/Library/Logs/Ryujinx",
+    "~/Library/Preferences/org.ryujinx.Ryujinx.plist",
+    "~/Library/Saved Application State/org.ryujinx.Ryujinx.savedState",
+  ]
+end
--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # An unofficial Homebrew tap for unsigned casks
 
  We require casks in the official [homebrew-cask](https://github.com/Homebrew/homebrew-cask) repo to be code signed. This tap is a collection of useful casks that are not code signed for one reason or another.
+
+## List of included casks by topic
+
+### Gaming
+
+[`ryujinx`](https://ryujinx.org) (Nintendo Switch emulator)
