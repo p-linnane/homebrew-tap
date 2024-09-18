f7e34414c1e4e102fce5ce0bbb8786d47cddacf5
ふゆのほし★ <165941061+fuyunohoshi@users.noreply.github.com>
2024-09-18 14:25:39 -0700

    ryujinx 1.1.1394 (#132)

--- Casks/ryujinx.rb
+++ Casks/ryujinx.rb
@@ -1,6 +1,6 @@
 cask "ryujinx" do
-  version "1.1.1393"
-  sha256 "a9c6ce7d661cfdcc8f9f2328193db58dfdb421c051f70cdda84b169079979513"
+  version "1.1.1394"
+  sha256 "dca72e796044eda83ad95ce8a4b148848346783375f87b0a356d73ba35955800"
 
   url "https://github.com/Ryujinx/release-channel-master/releases/download/#{version}/ryujinx-#{version}-macos_universal.app.tar.gz",
       verified: "github.com/Ryujinx/release-channel-master/"
