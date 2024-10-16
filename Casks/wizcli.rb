5b1b89fe5b37b58c41606bf12a4cca6e21db11ce
ふゆのほし★ <165941061+fuyunohoshi@users.noreply.github.com>
2024-10-16 12:22:57 -0700

    wizcli 0.52.0 (#149)
    Co-authored-by: Patrick Linnane <patrick@linnane.io>

--- Casks/wizcli.rb
+++ Casks/wizcli.rb
@@ -1,9 +1,9 @@
 cask "wizcli" do
   arch arm: "arm64", intel: "amd64"
 
-  version "0.51.0"
-  sha256 arm:   "d997954d6ab01572b0cd405f3d86de59b35d4085a885a3da1eedd9a7087c9d1c",
-         intel: "ecf975c1f8316a21d788a14d5df7835131fca1291cd33840eb95c472574156b1"
+  version "0.52.0"
+  sha256 arm:   "fee84f7e5525e8cb4d7df7b136b4bc9261ee0a7ac2b72ca122e8b2a36c96df23",
+         intel: "23a0ed854acb2028d0e33ac260f7c653639811f51b3aac29d1d1eca30753a5e6"
 
   url "https://downloads.wiz.io/wizcli/latest/wizcli-darwin-#{arch}"
   name "Wiz CLI"
