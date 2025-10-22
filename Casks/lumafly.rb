cbbe5d81970952e61ae9a7aaf4631034e6d308f8
Patrick Linnane <patrick@linnane.io>
2025-10-22 09:33:43 -0700

    lumafly 3.3.0.0 (new cask)
    Signed-off-by: Patrick Linnane <patrick@linnane.io>

---
+++ Casks/lumafly.rb
@@ -0,0 +1,17 @@
+cask "lumafly" do
+  version "3.3.0.0"
+  sha256 "2e45dee4ca450bd46926540c19e98b9a452759eaa233cf5d5d258c2dc2866f0d"
+
+  url "https://github.com/TheMulhima/Lumafly/releases/download/v#{version}/Lumafly-MacOS.zip",
+      verified: "github.com/TheMulhima/Lumafly/"
+  name "lumafly"
+  desc "Mod manager for Hollow Knight"
+  homepage "https://themulhima.github.io/Lumafly/"
+
+  app "Lumafly.app"
+
+  caveats <<~EOS
+    This software is not signed and does not meet macOS Gatekeeper requirements.
+    Running it requires bypassing built-in macOS protections, which reduces system security.
+  EOS
+end
