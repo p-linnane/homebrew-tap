76d600090cf54808bc32ceb7a288386e67e3cabd
Patrick Linnane <patrick@linnane.io>
2025-12-09 16:09:13 -0800

    apple-container 0.7.1
    Signed-off-by: Patrick Linnane <patrick@linnane.io>

--- Casks/apple-container.rb
+++ Casks/apple-container.rb
@@ -1,8 +1,8 @@
 cask "apple-container" do
-  version "0.6.0"
-  sha256 "4462fa6bc118fab382e44afdcc77db32b607418e0bc80a4e50f7053f585a3866"
+  version "0.7.1"
+  sha256 "dab7e1f0e0f41f9050909b27493f3ce94d9dc9fe1a79d26f090ce227d613c671"
 
-  url "https://github.com/apple/container/releases/download/#{version}/container-#{version}-installer-signed.pkg"
+  url "https://github.com/apple/container/releases/download/#{version}/container-installer-signed.pkg"
   name "container"
   desc "Create and run Linux containers using lightweight virtual machines"
   homepage "https://github.com/apple/container"
@@ -15,7 +15,7 @@ cask "apple-container" do
   depends_on arch: :arm64
   depends_on macos: ">= :sequoia"
 
-  pkg "container-#{version}-installer-signed.pkg"
+  pkg "container-installer-signed.pkg"
 
   # container APIs aren't guaranteed to be backward compatible,
   # so we stop the system service to ensure no components are out of sync.
